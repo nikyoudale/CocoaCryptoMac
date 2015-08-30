2a49f93fe9ada6255014593908a8395dd12f85d8
Nik Youdale <nik.youdale@gmail.com>
2015-08-30 11:07:33 +1000

    Initial commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocoaCryptoMac
+//
+//  Created by Nik Youdale on 30/08/2015.
+//  Copyright (c) 2015 Nik Youdale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
